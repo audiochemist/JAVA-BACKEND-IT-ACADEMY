53469a01fbf51df2ecfb73af7325e3751ce49d37
audiochemist <germanmedinacalle@gmail.com>
2024-02-07 15:17:52 +0100

    changes ignore

--- S1/S1_T4/src/main/java/N1Ex3/OutOfBoundsException.java
+++ S1/S1_T4/src/main/java/N1Ex3/OutOfBoundsException.java
@@ -5,11 +5,11 @@ import java.util.ArrayList;
 public class OutOfBoundsException {
 
 
-    public static void outOfBoundsException() {
+    public static void outOfBoundsException() throws ArrayIndexOutOfBoundsException {
     int index = 0;
 
       ArrayList<Integer> names = new ArrayList<Integer>();
-      if (index <= names.size()) {
+      if (index < names.size()) {
         throw new ArrayIndexOutOfBoundsException();
     }
 }
